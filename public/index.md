46bcd885b24517e82121eeb80a0ac4980e229f59
Lucas Cordiviola <lucarda27@hotmail.com>
2021-11-26 09:37:22 -0300

    delete content

--- public/index.md
+++
@@ -1,5 +0,0 @@
-# Pure Data
-
-<a class="green" href="http://puredata.info/downloads">Get Pure-Data</a>
-
-[Welcome to Pd](./welcome/index.html)
--- public/md2html.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-set cw=%~dp0
-
-FORFILES /m *.md /s /C "cmd /c pandoc -f markdown -t html5 @FILE --template %cw%template.txt -o @FNAME.html"
-
